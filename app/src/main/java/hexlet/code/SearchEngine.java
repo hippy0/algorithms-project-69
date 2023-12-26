9385e814621916a5473c271149d80ae41275be5c
hippy0 <realhippy0@gmail.com>
2023-12-26 23:38:16 +0600

    Fix linter errors

--- app/src/main/java/hexlet/code/SearchEngine.java
+++ app/src/main/java/hexlet/code/SearchEngine.java
@@ -1,6 +1,8 @@
 package hexlet.code;
 
-import java.util.*;
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
